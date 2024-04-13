83bd7212052863102bb132df2eac4cb81eedc542
gaurav <gauravpathak9058@gmail.com>
2024-04-13 12:05:54 +0530

    first commit

--- constants/index.js
+++ constants/index.js
@@ -33,10 +33,8 @@ export const sidebarLinks = [
 
 export const profileTabs = [
   { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
-  { value: "followers", label: "Followers", icon: "/assets/members.svg" },
-  { value: "following", label: "Following", icon: "/assets/members.svg" },
-  // { value: "replies", label: "Replies", icon: "/assets/members.svg" },
-  // { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
+  { value: "replies", label: "Replies", icon: "/assets/members.svg" },
+  { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
 ];
 
 export const communityTabs = [
